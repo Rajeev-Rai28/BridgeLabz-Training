202d06844ba8e150eaf7d3a6479125a14efcbe31
Rajeev Rai <rr6921727@gmai.com>
2025-12-17 15:58:01 +0530

    [Rajeev] : added a program two numbers addition

---
+++ gcr-codebase/program-prerequites/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class AddTwoNumbers {
+      public static void main(String [] args){
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter your 1st Num : ");
+	int num1 = sc.nextInt();
+	System.out.print("Enter your 2nd Num : ");
+	int num2 = sc.nextInt();
+	int sum = num1 + num2;
+	System.out.print("The addition of two numbers : "+sum);
+	}
+}
+
+			
