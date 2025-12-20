c8cd2975eb8a4960164f76d9a3d1124286c8ffdf
Rajeev Rai <rr6921727@gmai.com>
2025-12-20 07:59:02 +0530

    [Rajeev] : Added a program which print a year leap year or not

---
+++ gcr-codebase/control-flows/level3/LeapYearWithMulipleCondition.java
@@ -0,0 +1,35 @@
+// Creating Class with name LeapYearWithMulipleCondition indicating the purpose is to 
+// check a Leap year or not 
+
+import java.util.Scanner;
+public class LeapYearWithMulipleCondition {
+	public static void main(String [] args){
+	
+	//creating a scanner object
+	Scanner sc = new Scanner(System.in);
+	
+	//Taking input a number
+	System.out.print("Enter a Year : ");
+	int year = sc.nextInt();
+
+	//checking a year
+	while(year>=1582){
+	if(year % 100 == 0 && year % 400 == 0){
+	   System.out.println(year +" LeapYear");
+	   break;
+	 }
+	else if(year % 4 == 0 && year % 400 == 0){
+	   System.out.println(year +" LeapYear");
+	   break;
+	 }
+	else {
+	   System.out.println(year +" is not LeapYear");
+	   break;
+	 }
+	}
+	sc.close();
+	}
+}
+
+
+	  
