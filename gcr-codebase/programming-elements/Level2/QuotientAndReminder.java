a77209d722b088a0d59ed29b00c123c31df1704e
Rajeev Rai <rr6921727@gmai.com>
2025-12-20 21:23:42 +0530

    [Rajeev] : Added a program which calculate a quotient and remainder

---
+++ gcr-codebase/programming-elements/Level2/QuotientAndReminder.java
@@ -0,0 +1,24 @@
+// Create a class QuotientAndReminder to find the quotient and remainder of two number
+
+import java.util.Scanner;
+public class QuotientAndReminder{
+	public static void main (String [] args){
+	
+	// create Scanner object
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Enter first number : ");
+	int number1 = sc.nextInt();
+	System.out.print("Enter second number : ");
+	int number2 = sc.nextInt();
+	
+	int quotient = number1 / number2;
+	int remainder = number1 % number2;
+	
+	//display a result	
+	System.out.print("The Quotient is "+quotient+" and Reminder is "+remainder+" of two number "+number1+" and "+number2);
+		
+	sc.close();
+	
+	}
+}
