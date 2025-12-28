882dd9e347a16db8b098ac624f4c952e76ce8cdb
Rajeev Rai <rr6921727@gmai.com>
2025-12-28 22:12:58 +0530

    [Rajeev] : Added a ProductMain class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/ProductMain.java
@@ -0,0 +1,17 @@
+package com.constructors.levelone;
+
+public class ProductMain {
+	
+	public static void main(String[] args) {
+
+        Product p1 = new Product("Laptop", 55000);
+        Product p2 = new Product("Mobile", 25000);
+        Product p3 = new Product("Headphones", 3000);
+
+        p1.displayProductDetails();
+        p2.displayProductDetails();
+        p3.displayProductDetails();
+
+        Product.displayTotalProducts();
+    }
+}
