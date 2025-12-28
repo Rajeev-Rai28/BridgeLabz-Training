e01ac2200465c8036328516115ee669c2a1d5d12
Rajeev Rai <rr6921727@gmai.com>
2025-12-28 23:00:04 +0530

    [Rajeev] : Added a Program SavingsAccount subclass of BankAccount class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/SavingsAccount.java
@@ -0,0 +1,23 @@
+package com.constructors.levelone;
+
+class SavingsAccount extends BankAccount {
+
+    double interestRate;
+
+    // Constructor
+    SavingsAccount(long accountNumber, String accountHolder,
+                   double balance, double interestRate) {
+        super(accountNumber, accountHolder, balance);
+        this.interestRate = interestRate;
+    }
+
+    // Method demonstrating access modifiers
+    void displaySavingsAccountDetails() {
+        System.out.println("Account Number : " + accountNumber);   // public
+        System.out.println("Account Holder : " + accountHolder);   // protected
+        System.out.println("Balance        : ₹" + getBalance());   // private via getter
+        System.out.println("Interest Rate  : " + interestRate + "%");
+        System.out.println("-----------------------------");
+    }
+}
+
