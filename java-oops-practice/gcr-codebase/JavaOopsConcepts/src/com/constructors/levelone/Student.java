2363d025e50181c8bffd61f56f4c9db0699d5cc1
Rajeev Rai <rr6921727@gmai.com>
2025-12-28 22:36:10 +0530

    [Rajeev] : Added a Program Student class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Student.java
@@ -0,0 +1,43 @@
+package com.constructors.levelone;
+
+public class Student{
+
+    // Public variable
+    public int rollNumber;
+
+    // Protected variable
+    protected String name;
+
+    // Private variable
+    private double CGPA;
+
+    // Constructor
+    Student(int rollNumber, String name, double CGPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    // Public method to access CGPA
+    public double getCGPA() {
+        return CGPA;
+    }
+
+    // Public method to modify CGPA
+    public void setCGPA(double CGPA) {
+        if (CGPA >= 0.0 && CGPA <= 10.0) {
+            this.CGPA = CGPA;
+        } else {
+            System.out.println("Invalid CGPA value!");
+        }
+    }
+
+    // Display student details
+    public void displayStudentDetails() {
+        System.out.println("Roll Number : " + rollNumber);
+        System.out.println("Name        : " + name);
+        System.out.println("CGPA        : " + CGPA);
+        System.out.println("-------------------------");
+    }
+}
+
