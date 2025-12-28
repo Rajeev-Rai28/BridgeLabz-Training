0cc5ab012323b6cd84ba1cca2241d6b84a509954
Rajeev Rai <rr6921727@gmai.com>
2025-12-28 22:01:09 +0530

    [Rajeev] : Added a CarRentalMain class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/CarRentalMain.java
@@ -0,0 +1,11 @@
+package com.constructors.levelone;
+
+public class CarRentalMain {
+
+	public static void main(String[] args) {
+
+        CarRental rental1 = new CarRental("Rajeev Rai", "Hyundai Creta", 5);
+        rental1.displayDetails();
+    }
+
+}
