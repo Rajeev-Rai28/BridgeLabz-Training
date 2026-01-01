514abc8cbe42431bcdedcecc885d072680260c08
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 12:46:59 +0530

    [Rajeev] : Added a Program VehicleMain class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridInheritance/VehicleMain.java
@@ -0,0 +1,18 @@
+package com.inheritance.hybridInheritance;
+
+public class VehicleMain {
+    public static void main(String[] args) {
+
+        ElectricVehicle ev = new ElectricVehicle(150, "Tesla Model 3");
+        PetrolVehicle pv = new PetrolVehicle(180, "Honda City");
+
+        ev.displayInfo();
+        ev.charge();
+
+        System.out.println();
+
+        pv.displayInfo();
+        pv.refuel();
+    }
+}
+
