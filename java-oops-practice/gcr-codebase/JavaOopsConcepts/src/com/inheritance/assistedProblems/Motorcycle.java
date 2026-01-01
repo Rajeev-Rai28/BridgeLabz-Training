400caf9845ee86fd3bcad80a6c6a8e28a182f170
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 10:13:27 +0530

    [Rajeev] : Added a program Mototrcycle subClass of Vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedProblems/Motorcycle.java
@@ -0,0 +1,18 @@
+package com.inheritance.assistedProblems;
+
+public class Motorcycle extends Vehicle {
+    boolean hasGear;
+
+    Motorcycle(int maxSpeed, String fuelType, boolean hasGear) {
+        super(maxSpeed, fuelType);
+        this.hasGear = hasGear;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Gear System: " + (hasGear ? "Yes" : "No"));
+        System.out.println("Vehicle Type: Motorcycle");
+    }
+}
+
