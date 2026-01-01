add5d74943edec80337af1c66b281a60fe402e3f
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 11:06:07 +0530

    [Rajeev] : Added a program CheckingAccount subClass of BankAccount super class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalInheritance/CheckingAccount.java
@@ -0,0 +1,17 @@
+package com.inheritance.hierarchicalInheritance;
+
+public class CheckingAccount extends BankAccount {
+    double withdrawalLimit;
+
+    CheckingAccount(String accountNumber, double balance, double withdrawalLimit) {
+        super(accountNumber, balance);
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    void displayAccountType() {
+        System.out.println("Account Type: Checking Account");
+        displayBasicInfo();
+        System.out.println("Withdrawal Limit: ₹" + withdrawalLimit);
+    }
+}
+
