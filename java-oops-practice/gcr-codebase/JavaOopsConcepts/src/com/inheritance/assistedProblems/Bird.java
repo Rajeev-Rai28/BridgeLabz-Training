855bbc15aec3a0b3be30ae8d936dda2ef84c5ada
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 01:27:17 -0300

    [Rajeev] : Added a program Bird subclass of Animal superClass

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedProblems/Bird.java
@@ -0,0 +1,14 @@
+package com.inheritance.assistedProblems;
+
+public class Bird extends Animal {
+
+    Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Bird chirps");
+    }
+}
+
