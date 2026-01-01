bf8a269c1a42f848104a23efe77aaf9da7e5761e
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 11:21:48 +0530

    [Rajeev] : Added a program Person super class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalInheritance/Person.java
@@ -0,0 +1,18 @@
+package com.inheritance.hierarchicalInheritance;
+
+//Superclass
+public class Person {
+ String name;
+ int age;
+
+ Person(String name, int age) {
+     this.name = name;
+     this.age = age;
+ }
+
+ void displayBasicInfo() {
+     System.out.println("Name: " + name);
+     System.out.println("Age: " + age);
+ }
+}
+
