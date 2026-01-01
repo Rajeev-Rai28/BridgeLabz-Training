2f8f1ebc0fec3e1665eb88d195f02e36d5cff0e0
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 11:24:19 +0530

    [Rajeev] : Added a program Teacher subClass of Person super class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalInheritance/Teacher.java
@@ -0,0 +1,17 @@
+package com.inheritance.hierarchicalInheritance;
+
+public class Teacher extends Person {
+    String subject;
+
+    Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    void displayRole() {
+        System.out.println("Role: Teacher");
+        displayBasicInfo();
+        System.out.println("Subject: " + subject);
+    }
+}
+
