63b1f8454b20d7a44c1885e0093add9da797b0ab
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 02:07:58 -0300

    [Rajeev] : Added a program Course Base class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelInheritance/Course.java
@@ -0,0 +1,18 @@
+package com.inheritance.multilevelInheritance;
+
+//Base class
+public class Course {
+ String courseName;
+ int duration; // in hours
+
+ Course(String courseName, int duration) {
+     this.courseName = courseName;
+     this.duration = duration;
+ }
+
+ void displayDetails() {
+     System.out.println("Course Name: " + courseName);
+     System.out.println("Duration: " + duration + " hours");
+ }
+}
+
