77db6bfed6158b70131c0039ca8b5530ecb821d4
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 11:25:27 +0530

    [Rajeev] : Added a program Staff subClass of Person super class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalInheritance/Staff.java
@@ -0,0 +1,16 @@
+package com.inheritance.hierarchicalInheritance;
+
+public class Staff extends Person {
+    String department;
+
+    Staff(String name, int age, String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+    void displayRole() {
+        System.out.println("Role: Staff");
+        displayBasicInfo();
+        System.out.println("Department: " + department);
+    }
+}
