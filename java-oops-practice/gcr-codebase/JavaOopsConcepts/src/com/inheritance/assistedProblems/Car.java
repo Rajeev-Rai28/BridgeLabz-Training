2dd2477c9a754cf22437889d71b6d882c7efec5a
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 10:12:12 +0530

    [Rajeev] : Added a program Car subClass of Vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedProblems/Car.java
@@ -0,0 +1,18 @@
+package com.inheritance.assistedProblems;
+
+public class Car extends Vehicle {
+    int seatCapacity;
+
+    Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+        System.out.println("Vehicle Type: Car");
+    }
+}
+
