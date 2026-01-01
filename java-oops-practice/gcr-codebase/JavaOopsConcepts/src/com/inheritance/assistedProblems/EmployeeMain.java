b35b7b3e53c4e3a8250f0a137453954965799f0d
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 01:35:03 -0300

    [Rajeev] : Added a program EmployeeMain class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedProblems/EmployeeMain.java
@@ -0,0 +1,19 @@
+package com.inheritance.assistedProblems;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+
+        Employee e1 = new Manager("Rajeev", 101, 80000, 10);
+        Employee e2 = new Developer("Amit", 102, 60000, "Java");
+        Employee e3 = new Intern("Neha", 103, 15000, 6);
+
+        e1.displayDetails();
+        System.out.println();
+
+        e2.displayDetails();
+        System.out.println();
+
+        e3.displayDetails();
+    }
+}
+
