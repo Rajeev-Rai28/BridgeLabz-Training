e1ce0eed9b01539512bceee3c5fca57147d314d5
Rajeev Rai <rr6921727@gmai.com>
2026-01-01 02:09:14 -0300

    [Rajeev] : Added a program PaidOnlineCourse subClass of Course Base class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelInheritance/PaidOnlineCourse.java
@@ -0,0 +1,26 @@
+package com.inheritance.multilevelInheritance;
+
+public class PaidOnlineCourse extends OnlineCourse {
+    double fee;
+    double discount; // percentage
+
+    PaidOnlineCourse(String courseName, int duration, String platform,
+                     boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    double calculateFinalFee() {
+        return fee - (fee * discount / 100);
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Course Fee: ₹" + fee);
+        System.out.println("Discount: " + discount + "%");
+        System.out.println("Final Fee: ₹" + calculateFinalFee());
+    }
+}
+
