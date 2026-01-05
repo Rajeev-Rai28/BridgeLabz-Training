744d42f0e12553977584de8301f58b59080e06ce
Rajeev Rai <rr6921727@gmai.com>
2026-01-05 16:43:54 +0530

    [Rajeev] : modify project because path

--- java-oops-practice/scenario-based-codebase/ScenarioProblems/src/com/day1/vehicleRentalApplication/VehicleRentApp.java
+++ java-oops-practice/scenario-based-codebase/ScenarioProblems/src/com/day1/vehicleRentalApplication/VehicleRentApp.java
@@ -1,7 +1,7 @@
 package com.day1.vehicleRentalApplication;
 
 public class VehicleRentApp {
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 
         Customer customer = new Customer("Ayush", 101);
 
