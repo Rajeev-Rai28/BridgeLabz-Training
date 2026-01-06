f59c30da4649cf8a1e8ffd3a29dab6a6a15061bc
Rajeev Rai <rr6921727@gmai.com>
2026-01-06 22:55:49 +0530

    [Rajeev] : Check for a Pair with Given Sum in an Array

---
+++ dsa-practice/gcr-codebase/JavaDSAPractice/src/com/hashmapandhashfunctions/CheckforaPairwithGivenSuminanArray/PairWithGivenSumMain.java
@@ -0,0 +1,11 @@
+package com.hashmapandhashfunctions.CheckforaPairwithGivenSuminanArray;
+
+public class PairWithGivenSumMain {
+    public static void main(String[] args) {
+
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        PairWithGivenSum.hasPair(arr, target);
+    }
+}
