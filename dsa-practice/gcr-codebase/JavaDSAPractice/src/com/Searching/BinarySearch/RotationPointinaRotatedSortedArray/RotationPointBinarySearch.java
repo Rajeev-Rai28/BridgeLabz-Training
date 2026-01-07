ff1a302623daea723c8fe8a9d63caaf0d6c748a7
Rajeev Rai <rr6921727@gmai.com>
2026-01-07 11:55:59 +0530

    [Rajeev] : Added a Rotation Point in a Rotated Sorted Array

---
+++ dsa-practice/gcr-codebase/JavaDSAPractice/src/com/Searching/BinarySearch/RotationPointinaRotatedSortedArray/RotationPointBinarySearch.java
@@ -0,0 +1,33 @@
+package com.Searching.BinarySearch.RotationPointinaRotatedSortedArray;
+
+
+public class RotationPointBinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {15, 18, 2, 3, 6, 12};
+
+        int rotationIndex = findRotationPoint(arr);
+
+        System.out.println("Rotation Point Index: " + rotationIndex);
+        System.out.println("Smallest Element: " + arr[rotationIndex]);
+    }
+
+    public static int findRotationPoint(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left; 
+    }
+}
