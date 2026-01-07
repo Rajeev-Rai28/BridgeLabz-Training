2d440926b7c53544d6430f52c98ecea33d682164
Rajeev Rai <rr6921727@gmai.com>
2026-01-07 10:08:39 +0530

    [Rajeev] : Reverse a String Using StringBuilder

---
+++ dsa-practice/gcr-codebase/JavaDSAPractice/src/com/StringBuilder/RemoveDuplicatesfromaString/RemoveDuplicatesUsingStringBuilder.java
@@ -0,0 +1,29 @@
+package com.StringBuilder.RemoveDuplicatesfromaString;
+
+import java.util.HashSet;
+
+public class RemoveDuplicatesUsingStringBuilder {
+
+    public static void main(String[] args) {
+
+        String input = "programming";
+
+        String result = removeDuplicates(input);
+
+        System.out.println("String without duplicates: " + result);
+    }
+
+    public static String removeDuplicates(String str) {
+
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> seen = new HashSet<>();
+
+        for (char ch : str.toCharArray()) {
+            if (!seen.contains(ch)) {
+                seen.add(ch);
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
