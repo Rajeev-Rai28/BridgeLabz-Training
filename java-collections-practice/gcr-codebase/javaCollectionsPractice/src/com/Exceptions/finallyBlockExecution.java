3d4774584108ae1ac7436e0f02f8d5066496d1c7
Rajeev Rai <rr6921727@gmai.com>
2026-01-26 23:54:21 +0530

    [Rajeev] : Added a finallyBlockExecution java code

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/Exceptions/finallyBlockExecution.java
@@ -0,0 +1,28 @@
+package com.Exceptions;
+
+import java.util.Scanner;
+
+public class finallyBlockExecution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } 
+        catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero.");
+        } 
+        finally {
+            System.out.println("Operation completed");
+            sc.close();
+        }
+    }
+}
