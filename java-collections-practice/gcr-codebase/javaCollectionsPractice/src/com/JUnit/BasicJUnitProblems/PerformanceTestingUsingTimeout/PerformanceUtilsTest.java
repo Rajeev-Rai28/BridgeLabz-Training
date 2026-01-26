dd725c9e09797505316d4a0c9ceaa1d365402d3c
Rajeev Rai <rr6921727@gmai.com>
2026-01-27 01:01:18 +0530

    [Rajeev] : Added a PerformanceUtilsTest java code

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/JUnit/BasicJUnitProblems/PerformanceTestingUsingTimeout/PerformanceUtilsTest.java
@@ -0,0 +1,16 @@
+package com.JUnit.BasicJUnitProblems.PerformanceTestingUsingTimeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+public class PerformanceUtilsTest {
+
+    PerformanceUtils utils = new PerformanceUtils();
+
+    @Test
+    @Timeout(2)   
+    void testLongRunningTaskTimeout() throws InterruptedException {
+        utils.longRunningTask();
+    }
+}
