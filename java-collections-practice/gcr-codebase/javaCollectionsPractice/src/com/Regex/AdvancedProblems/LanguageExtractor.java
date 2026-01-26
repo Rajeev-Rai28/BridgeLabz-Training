ef9d6c13d99319d7ad0a4de11a9fbc59be3d1acd
Rajeev Rai <rr6921727@gmai.com>
2026-01-27 00:09:25 +0530

    [Rajeev] : Added a LanguageExtractor java code

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/Regex/AdvancedProblems/LanguageExtractor.java
@@ -0,0 +1,18 @@
+package com.Regex.AdvancedProblems;
+
+import java.util.regex.*;
+
+public class LanguageExtractor {
+    public static void main(String[] args) {
+
+        String text = "I love Java, Python, and JavaScript, but I haven't tried Go yet.";
+
+        String regex = "\\b(Java|Python|JavaScript|Go)\\b";
+
+        Matcher m = Pattern.compile(regex).matcher(text);
+
+        while(m.find()) {
+            System.out.println(m.group());
+        }
+    }
+}
