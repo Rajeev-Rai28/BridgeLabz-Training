982bc3cb1543451fb6e0f8e09a77b5aefed83042
Rajeev Rai <rr6921727@gmai.com>
2026-01-27 00:10:05 +0530

    [Rajeev] : Added a RepeatingWords java code

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/Regex/AdvancedProblems/RepeatingWords.java
@@ -0,0 +1,18 @@
+package com.Regex.AdvancedProblems;
+
+import java.util.regex.*;
+
+public class RepeatingWords {
+    public static void main(String[] args) {
+
+        String text = "This is is a repeated repeated word test.";
+
+        String regex = "\\b(\\w+)\\s+\\1\\b";
+
+        Matcher m = Pattern.compile(regex).matcher(text);
+
+        while(m.find()) {
+            System.out.println(m.group(1));
+        }
+    }
+}
