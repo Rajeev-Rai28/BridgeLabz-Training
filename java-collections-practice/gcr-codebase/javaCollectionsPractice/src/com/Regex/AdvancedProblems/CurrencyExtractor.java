73b04b086e8d1b5261689b63a1092d94559f3a25
Rajeev Rai <rr6921727@gmai.com>
2026-01-27 00:08:07 +0530

    [Rajeev] : Added a CurrencyExtractor java code

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/Regex/AdvancedProblems/CurrencyExtractor.java
@@ -0,0 +1,18 @@
+package com.Regex.AdvancedProblems;
+
+import java.util.regex.*;
+
+public class CurrencyExtractor {
+    public static void main(String[] args) {
+
+        String text = "The price is $45.99, and the discount is 10.50.";
+
+        String regex = "\\$?\\d+(\\.\\d{2})?";
+
+        Matcher m = Pattern.compile(regex).matcher(text);
+
+        while(m.find()) {
+            System.out.println(m.group());
+        }
+    }
+}
