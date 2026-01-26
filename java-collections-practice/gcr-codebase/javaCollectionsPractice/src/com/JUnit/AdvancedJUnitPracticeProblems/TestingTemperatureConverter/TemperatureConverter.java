1c8b6f59104ab68c15cc8879401b24951ff5c0b9
Rajeev Rai <rr6921727@gmai.com>
2026-01-27 00:53:48 +0530

    [Rajeev] : Added a TemperatureConverter java code

--- java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/JUnit/AdvancedJUnitPracticeProblems/TestingTemperatureConverter/TemperatureConverter.java
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/JUnit/AdvancedJUnitPracticeProblems/TestingTemperatureConverter/TemperatureConverter.java
@@ -2,4 +2,11 @@ package com.JUnit.AdvancedJUnitPracticeProblems.TestingTemperatureConverter;
 
 public class TemperatureConverter {
 
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
 }
