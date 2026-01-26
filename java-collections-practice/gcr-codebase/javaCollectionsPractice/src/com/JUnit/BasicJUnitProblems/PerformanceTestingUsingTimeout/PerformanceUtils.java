1bb672b16747ea8c00dd838d07d3c47a5304d9e2
Rajeev Rai <rr6921727@gmai.com>
2026-01-27 01:01:57 +0530

    [Rajeev] : Added a PerformanceUtils java code

--- java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/JUnit/BasicJUnitProblems/PerformanceTestingUsingTimeout/PerformanceUtils.java
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/JUnit/BasicJUnitProblems/PerformanceTestingUsingTimeout/PerformanceUtils.java
@@ -2,4 +2,8 @@ package com.JUnit.BasicJUnitProblems.PerformanceTestingUsingTimeout;
 
 public class PerformanceUtils {
 
+    public String longRunningTask() throws InterruptedException {
+        Thread.sleep(3000);  
+        return "Done";
+    }
 }
