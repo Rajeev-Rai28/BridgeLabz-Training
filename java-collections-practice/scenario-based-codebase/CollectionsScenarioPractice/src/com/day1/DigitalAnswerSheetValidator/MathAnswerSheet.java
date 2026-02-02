8ca6986e924c104a898abc6bf1c79536048e3567
Rajeev Rai <rr6921727@gmai.com>
2026-02-02 12:43:17 +0530

    [Rajeev] : Added a MathAnswerSheet.java

---
+++ java-collections-practice/scenario-based-codebase/CollectionsScenarioPractice/src/com/day1/DigitalAnswerSheetValidator/MathAnswerSheet.java
@@ -0,0 +1,9 @@
+package com.day1.DigitalAnswerSheetValidator;
+
+import java.util.List;
+
+public class MathAnswerSheet extends AnswerSheet {
+    public MathAnswerSheet(List<String> answers) {
+        super(answers);
+    }
+}
