b9e081d550fecb950e323d39c222faf0b63fa682
Rajeev Rai <rr6921727@gmai.com>
2026-02-02 12:50:48 +0530

    [Rajeev] : Added a InvalidResumeFormatException.java

---
+++ java-collections-practice/scenario-based-codebase/CollectionsScenarioPractice/src/com/day1/SmartResumeFilteringSystem/InvalidResumeFormatException.java
@@ -0,0 +1,7 @@
+package com.day1.SmartResumeFilteringSystem;
+
+public class InvalidResumeFormatException extends Exception {
+    public InvalidResumeFormatException(String message) {
+        super(message);
+    }
+}
