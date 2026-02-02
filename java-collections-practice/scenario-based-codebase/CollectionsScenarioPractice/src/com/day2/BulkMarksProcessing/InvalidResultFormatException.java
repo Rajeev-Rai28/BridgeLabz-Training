c2b4f558e134bef58c47a48c9b88f1f162034468
Rajeev Rai <rr6921727@gmai.com>
2026-02-02 13:02:46 +0530

    [Rajeev] : Added a InvalidResultFormatException.java

---
+++ java-collections-practice/scenario-based-codebase/CollectionsScenarioPractice/src/com/day2/BulkMarksProcessing/InvalidResultFormatException.java
@@ -0,0 +1,7 @@
+package com.day2.BulkMarksProcessing;
+
+public class InvalidResultFormatException extends Exception {
+    public InvalidResultFormatException(String msg) {
+        super(msg);
+    }
+}
