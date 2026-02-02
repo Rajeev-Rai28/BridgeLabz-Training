0960b04bb5b766243e8532e8b2718489b8b87cf2
Rajeev Rai <rr6921727@gmai.com>
2026-02-02 12:44:00 +0530

    [Rajeev] : Added a ScienceAnswerSheet.java

---
+++ java-collections-practice/scenario-based-codebase/CollectionsScenarioPractice/src/com/day1/DigitalAnswerSheetValidator/ScienceAnswerSheet.java
@@ -0,0 +1,9 @@
+package com.day1.DigitalAnswerSheetValidator;
+
+import java.util.List;
+
+public class ScienceAnswerSheet extends AnswerSheet {
+    public ScienceAnswerSheet(List<String> answers) {
+        super(answers);
+    }
+}
