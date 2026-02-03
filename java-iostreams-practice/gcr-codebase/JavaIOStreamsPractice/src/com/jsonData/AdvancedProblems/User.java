51432be8ee527802fd743ebead198e5ffd511878
Rajeev Rai <rr6921727@gmai.com>
2026-02-03 10:04:41 +0530

    [Rajeev] : Added a User.java

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamsPractice/src/com/jsonData/AdvancedProblems/User.java
@@ -0,0 +1,11 @@
+package com.jsonData.AdvancedProblems;
+
+class User {
+    public String name;
+    public int age;
+
+    User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
