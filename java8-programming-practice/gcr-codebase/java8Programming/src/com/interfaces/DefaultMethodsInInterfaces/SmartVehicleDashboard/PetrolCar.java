85fc262c1860dbb492786f527d13faeef9be5a34
Rajeev Rai <rr6921727@gmai.com>
2026-02-04 20:46:53 +0530

    [Rajeev] : Added a PetrolCar.java

---
+++ java8-programming-practice/gcr-codebase/java8Programming/src/com/interfaces/DefaultMethodsInInterfaces/SmartVehicleDashboard/PetrolCar.java
@@ -0,0 +1,7 @@
+package com.interfaces.DefaultMethodsInInterfaces.SmartVehicleDashboard;
+
+class PetrolCar implements Vehicle {
+    public void displaySpeed() {
+        System.out.println("Speed: 80 km/h");
+    }
+}
