f15a0743ad17e6deeec5be260e4479ae7091a6d0
Rajeev Rai <rr6921727@gmai.com>
2026-02-04 17:21:21 +0530

    [Rajeev] : Added a UnitConversionTool/UnitConverter.java

---
+++ java8-programming-practice/gcr-codebase/java8Programming/src/com/interfaces/StaticMethodsInInterfaces/UnitConversionTool/UnitConverter.java
@@ -0,0 +1,12 @@
+package com.interfaces.StaticMethodsInInterfaces.UnitConversionTool;
+
+interface UnitConverter {
+
+    static double kmToMiles(double km) {
+        return km * 0.621371;
+    }
+
+    static double kgToLbs(double kg) {
+        return kg * 2.20462;
+    }
+}
