6bacb76d58ddb886a35cf7b2ee7205664d7b86c5
Rajeev Rai <rr6921727@gmai.com>
2026-02-04 17:29:46 +0530

    [Rajeev] : Added a BackgroundJobExecution.java

---
+++ java8-programming-practice/gcr-codebase/java8Programming/src/com/interfaces/FunctionalInterfaces/BackgroundJobExecution.java
@@ -0,0 +1,22 @@
+package com.interfaces.FunctionalInterfaces;
+
+public class BackgroundJobExecution {
+
+    public static void main(String[] args) {
+
+        Runnable job = () -> {
+            System.out.println("Background job started...");
+            try{
+                Thread.sleep(2000);
+            }catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Background job completed");
+        };
+
+        Thread thread = new Thread(job);
+        thread.start();
+
+        System.out.println("Main thread continues...");
+    }
+}
