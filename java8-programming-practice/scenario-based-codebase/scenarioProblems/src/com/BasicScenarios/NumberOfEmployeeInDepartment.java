9d8550dea0bca6571eed210e934d0727aaf8db22
Rajeev Rai <rr6921727@gmai.com>
2026-02-07 16:40:50 +0530

    [Rajeev] : Added a NumberOfEmployeeInDepartment.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/BasicScenarios/NumberOfEmployeeInDepartment.java
@@ -0,0 +1,23 @@
+package com.BasicScenarios;
+import java.util.*;
+import java.util.stream.*;
+
+public class NumberOfEmployeeInDepartment {
+	public static void main(String[] args) {
+		List<Employee> employees = Arrays.asList(
+                new Employee(1, "Harshal", 23, "Male", "IT", 2022, 50000),
+                new Employee(2, "Neha", 25, "Female", "HR", 2021, 45000),
+                new Employee(3, "Amit", 28, "Male", "Finance", 2020, 60000),
+                new Employee(4, "Zoya", 26, "Female", "IT", 2023, 52000),
+                new Employee(5, "Rahul", 30, "Male", "HR", 2019, 58000)
+        );
+
+		Map<String, Long> employeeCountByDept = employees.stream()
+		                 .collect(Collectors.groupingBy(Employee::getDepartment,
+		                         Collectors.counting()
+		                 ));
+
+		employeeCountByDept.forEach((dept, count) -> System.out.println(dept + " : " + count));
+
+	}
+}
