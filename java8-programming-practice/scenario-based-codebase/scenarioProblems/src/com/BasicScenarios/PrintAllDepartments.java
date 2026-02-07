431f2250e95b80364f438150fb83725a1e83828c
Rajeev Rai <rr6921727@gmai.com>
2026-02-07 16:39:05 +0530

    [Rajeev] : Added a PrintAllDepartments.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/BasicScenarios/PrintAllDepartments.java
@@ -0,0 +1,24 @@
+package com.BasicScenarios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrintAllDepartments {
+	public static void main(String[] args) {
+
+        List<Employee> employees = Arrays.asList(
+                new Employee(1, "Harshal", 23, "Male", "IT", 2022, 50000),
+                new Employee(2, "Neha", 25, "Female", "HR", 2021, 45000),
+                new Employee(3, "Amit", 28, "Male", "Finance", 2020, 60000),
+                new Employee(4, "Zoya", 26, "Female", "IT", 2023, 52000),
+                new Employee(5, "Rahul", 30, "Male", "HR", 2019, 58000)
+        );
+
+        //Print all unique department names
+        employees.stream()
+                 .map(Employee::getDepartment)
+                 .distinct()
+                 .forEach(System.out::println);
+
+    }
+}
