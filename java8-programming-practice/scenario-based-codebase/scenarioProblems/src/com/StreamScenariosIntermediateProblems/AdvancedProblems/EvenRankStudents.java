acbbacc32d405566eac9039be5cf9f47bdf2f63e
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 20:46:04 +0530

    [Rajeev] : Added a EvenRankStudents.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/StreamScenariosIntermediateProblems/AdvancedProblems/EvenRankStudents.java
@@ -0,0 +1,14 @@
+package com.StreamScenariosIntermediateProblems.AdvancedProblems;
+
+import java.util.*;
+
+public class EvenRankStudents {
+
+    public static void main(String[] args) {
+
+        StudentData.getStudents()
+                .stream()
+                .filter(s -> s.getRank() % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
