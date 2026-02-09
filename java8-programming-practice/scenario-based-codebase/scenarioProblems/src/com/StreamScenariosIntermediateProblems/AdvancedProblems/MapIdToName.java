97cd011c751cb3bd341ae59da4cef4a07b0a65b1
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 20:49:31 +0530

    [Rajeev] : Added a MapIdToName.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/StreamScenariosIntermediateProblems/AdvancedProblems/MapIdToName.java
@@ -0,0 +1,19 @@
+package com.StreamScenariosIntermediateProblems.AdvancedProblems;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MapIdToName {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> map =
+                StudentData.getStudents()
+                        .stream()
+                        .collect(Collectors.toMap(
+                                Student::getId,
+                                Student::getName));
+
+        System.out.println(map);
+    }
+}
