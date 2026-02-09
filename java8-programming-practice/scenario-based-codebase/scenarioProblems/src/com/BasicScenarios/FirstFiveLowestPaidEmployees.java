8b165e7822f15efee48be7d9e72178610b6cd9a4
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 20:40:27 +0530

    [Rajeev] : Added a FirstFiveLowestPaidEmployees.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/BasicScenarios/FirstFiveLowestPaidEmployees.java
@@ -0,0 +1,25 @@
+package com.BasicScenarios;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class FirstFiveLowestPaidEmployees {
+	public static void main(String[] args) {
+		List<Employee> employees = Arrays.asList(
+	            new Employee(1, "Harshal", 23, "Male", "IT", 2022, 50000),
+	            new Employee(2, "Neha", 25, "Female", "HR", 2021, 45000),
+	            new Employee(3, "Amit", 28, "Male", "Finance", 2020, 60000),
+	            new Employee(4, "Zoya", 26, "Female", "IT", 2023, 52000),
+	            new Employee(5, "Rahul", 30, "Male", "HR", 2019, 58000)
+	        );
+		
+		List<Employee> lowestFivePaidEmployees = employees.stream()
+		                 .sorted(Comparator.comparingDouble(Employee::getSalary))
+		                 .limit(5)
+		                 .collect(Collectors.toList());
+
+		System.out.println("First Five Lowest Paid Employees:");
+		lowestFivePaidEmployees.forEach(System.out::println);
+
+	}
+}
