0ffaf28459dfb5e4e7769de44ac7efb4224679e5
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 21:54:48 +0530

    [Rajeev] : Added a StudentData.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/StreamScenariosIntermediateProblems/ConditionalAndMatching/StudentData.java
@@ -0,0 +1,30 @@
+package com.StreamScenariosIntermediateProblems.ConditionalAndMatching;
+
+import java.util.*;
+
+public class StudentData {
+
+    public static List<Student> getStudents() {
+
+        return Arrays.asList(
+
+            new Student(101,"Rohit Sharma","Rohit",20,"Male","Bhopal","CSE",1.2,
+                    Arrays.asList("9876543210","9123456789")),
+
+            new Student(102,"Aman Verma","Aman",22,"Male","Indore","ECE",2.5,
+                    Arrays.asList("9876543222")),
+
+            new Student(103,"Neha Singh","Neha",21,"Female","Bhopal","IT",1.8,
+                    Arrays.asList("9876543233","9988776655")),
+
+            new Student(104,"Priya Patel","Priya",23,"Female","Delhi","Biotech",3.1,
+                    Arrays.asList("9876543244")),
+
+            new Student(105,"Kunal Mehta","Kunal",19,"Male","Indore","Mechanical",0.9,
+                    Arrays.asList("9876543255","9000011111","8111122222")),
+
+            new Student(106,"Ankit Mishra","Ankit",24,"Male","Bhopal","ECE",4.2,
+                    Arrays.asList("9876543266"))
+        );
+    }
+}
