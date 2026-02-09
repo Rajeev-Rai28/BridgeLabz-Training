13bd142887fd20b6ad1c909516a57bf381157344
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 22:00:02 +0530

    [Rajeev] : Added a CountByGender.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/StreamScenariosIntermediateProblems/GenderBasedAnalysis/CountByGender.java
@@ -0,0 +1,20 @@
+package com.StreamScenariosIntermediateProblems.GenderBasedAnalysis;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CountByGender {
+
+    public static void main(String[] args) {
+
+        List<Student> students = StudentData.getStudents();
+
+        Map<String, Long> result =
+                students.stream()
+                        .collect(Collectors.groupingBy(
+                                Student::getGender,
+                                Collectors.counting()));
+
+        System.out.println("Male & Female Count: " + result);
+    }
+}
