0022b2b4026368ff42c86d5c46377d2ffa7778a0
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 20:51:24 +0530

    [Rajeev] : Added a PartitionByAge.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/StreamScenariosIntermediateProblems/BonusProblems/PartitionByAge.java
@@ -0,0 +1,17 @@
+package com.StreamScenariosIntermediateProblems.BonusProblems;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PartitionByAge {
+
+    public static void main(String[] args) {
+
+        Map<Boolean, List<Student>> map =
+                StudentData.getStudents().stream()
+                        .collect(Collectors.partitioningBy(s -> s.getAge() > 25));
+
+        System.out.println("Age > 25  : " + map.get(true));
+        System.out.println("Age <= 25 : " + map.get(false));
+    }
+}
