3ed716c0a9e3fd7ea80db9781bb75195ef82c3a4
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 20:52:25 +0530

    [Rajeev] : Added a Top3Youngest.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/StreamScenariosIntermediateProblems/BonusProblems/Top3Youngest.java
@@ -0,0 +1,14 @@
+package com.StreamScenariosIntermediateProblems.BonusProblems;
+
+import java.util.*;
+
+public class Top3Youngest {
+
+    public static void main(String[] args) {
+
+        StudentData.getStudents().stream()
+                .sorted(Comparator.comparingInt(Student::getAge))
+                .limit(3)
+                .forEach(System.out::println);
+    }
+}
