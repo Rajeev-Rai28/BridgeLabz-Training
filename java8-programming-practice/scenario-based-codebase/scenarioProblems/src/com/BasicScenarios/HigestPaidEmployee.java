200588e10cd031fae157dcf682988d5c4e8b1d81
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 20:39:58 +0530

    [Rajeev] : Added a HigestPaidEmployee.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/BasicScenarios/HigestPaidEmployee.java
@@ -0,0 +1,22 @@
+package com.BasicScenarios;
+
+import java.util.*;
+
+public class HigestPaidEmployee {
+	public static void main(String[] args) {
+
+        List<Employee> employees = Arrays.asList(
+                new Employee(1, "Harshal", 23, "Male", "IT", 2022, 50000),
+                new Employee(2, "Neha", 25, "Female", "HR", 2021, 45000),
+                new Employee(3, "Amit", 28, "Male", "Finance", 2020, 60000),
+                new Employee(4, "Zoya", 26, "Female", "IT", 2023, 52000),
+                new Employee(5, "Rahul", 30, "Male", "HR", 2019, 58000)
+        );
+
+        Optional<Employee> highestPaidEmployee = employees.stream()
+                         .max(Comparator.comparingDouble(Employee::getSalary));
+
+        System.out.println("\nHighest Paid Employee:");
+        highestPaidEmployee.ifPresent(System.out::println);
+    }
+}
