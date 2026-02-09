8ce11ec2c924d869cb6bedc03788f87dbdc7397e
Rajeev Rai <rr6921727@gmai.com>
2026-02-09 20:46:48 +0530

    [Rajeev] : Added a MapDeptToStudents.java

---
+++ java8-programming-practice/scenario-based-codebase/scenarioProblems/src/com/StreamScenariosIntermediateProblems/AdvancedProblems/MapDeptToStudents.java
@@ -0,0 +1,18 @@
+package com.StreamScenariosIntermediateProblems.AdvancedProblems;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MapDeptToStudents {
+
+    public static void main(String[] args) {
+
+        Map<String, List<Student>> map =
+                StudentData.getStudents()
+                        .stream()
+                        .collect(Collectors.groupingBy(
+                                Student::getDepartment));
+
+        map.forEach((k, v) -> System.out.println(k + " -> " + v));
+    }
+}
